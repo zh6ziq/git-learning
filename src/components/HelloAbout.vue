b8674bf18680b5e3df5d9a982d00f211887a1b55
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2021-01-12 18:21:33 +0800

    newBranch commit

---
+++ src/components/HelloAbout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>This is a new about page.</div>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloAbout />
   </div>
 </template>
+
+<script>
+import HelloAbout from "./../components/HelloAbout";
+export default {
+  components: {
+    HelloAbout,
+  },
+};
+</script>
