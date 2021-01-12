f4c8bdc0fa52637602e76612d945acf07e6f0f94
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2021-01-12 18:45:05 +0800

    commit a new components

--- src/components/HelloAbout.vue
+++ src/components/HelloAbout.vue
@@ -2,11 +2,14 @@
   <div>
     <div>This is a new about page.</div>
     <div>A new content here.</div>
+    <new-components />
   </div>
 </template>
 
 <script>
+import NewComponents from "./NewComponents.vue";
 export default {
+  components: { NewComponents },
   //
 };
 </script>
---
+++ src/components/NewComponents.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>A new component here.</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
